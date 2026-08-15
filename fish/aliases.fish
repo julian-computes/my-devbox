489a58160276c99f024f245d90d1fa965e82d8a3
Julian Fernandez <jufer002.work@gmail.com>
2026-08-15 17:40:44 -0400

    Add fish take command

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -4,6 +4,16 @@ alias gsw='git switch'
 alias gst='git status'
 alias gco='git checkout'
 
+# Create a directory, including parents, then enter it.
+function take --description 'Create a directory and enter it'
+    if test (count $argv) -ne 1
+        echo 'usage: take DIRECTORY' >&2
+        return 2
+    end
+
+    mkdir -p -- $argv[1]; and cd -- $argv[1]
+end
+
 function rebuild-my-devbox --description 'Rebuild NixOS from my-devbox'
     "$HOME/.config/my-devbox/scripts/bootstrap.sh"
 end
